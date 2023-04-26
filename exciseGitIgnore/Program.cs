82781724802f439d98a401972c43bf0121d461e9
Yiding Wang <yiding.wang82@gmail.com>
2023-04-26 10:19:05 -0400

    initial commit

---
+++ exciseGitIgnore/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace exciseGitIgnore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("123654789");
+        }
+    }
+}
